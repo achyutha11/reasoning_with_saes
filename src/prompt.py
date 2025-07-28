e5616c6cd4dd8344e52821638e87e4a82ffb4d53
Achu Menon <achyutha11@gmail.com>
2025-07-28 11:58:44 -0700

    complete draft of reasoning feature extraction

--- src/prompt.py
+++ src/prompt.py
@@ -1,8 +1,9 @@
 COT_PROMPT = r'Please reason step by step, and put your final answer within \boxed{}.'
 
-def format_prompt_aqua(query, reasoning=True, include_options=True):
-    question, options = query['question'], query['options']
-    joined_options = "\n".join(options) if include_options else ""
+def format_prompt_aqua(question, reasoning=True, include_options=True):
+    # question, options = query['question'], query['options']
+    # joined_options = "\n".join(options) if include_options else ""
+    joined_options = ""
     if reasoning:
         return f'<s>[INST] {question}{joined_options}\n{COT_PROMPT} [/INST] \n<think>\n'
     else:
