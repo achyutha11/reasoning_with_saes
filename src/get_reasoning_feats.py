c710652b2a1201de955b26b17bc01877fb4b780c
Achu Menon <achyutha11@gmail.com>
2025-07-28 11:58:44 -0700

    complete draft of reasoning feature extraction

--- src/get_reasoning_feats.py
+++ src/get_reasoning_feats.py
@@ -3,40 +3,73 @@ from model import load_model
 from sae import load_sae, get_sae_acts
 from datasets import load_aqua, load_trivia
 import torch
+from torch.utils.data import DataLoader, Dataset
 from tqdm import tqdm
 from scipy.stats import trim_mean
 import numpy as np
+from functools import partial
 
+class IndexedPromptDataset(Dataset):
+    def __init__(self, num_examples):
+        self.indices = list(range(num_examples))
+
+    def __len__(self):
+        return len(self.indices)
+
+    def __getitem__(self, idx):
+        return self.indices[idx]
+
+def collate_tokenized(batch_indices, tokenized):
+    return {k: v[batch_indices] for k, v in tokenized.items()}
+
+def get_ds_saes(sae, layer, prompts, model, collate_fn, batch_size=8, agg='mean'):
+
+    dataset = IndexedPromptDataset(len(prompts))
+    dataloader = DataLoader(dataset, batch_size=batch_size, collate_fn=collate_fn)
+
+    num_feats = sae.cfg.d_sae
+    sae_mat = torch.zeros(len(prompts), num_feats)
+
+    with torch.no_grad():
+        for i, batch_inputs in enumerate(tqdm(dataloader)):
+            batch_inputs = {k: v.to(model.device) for k, v in batch_inputs.items()}
+            batch_feats = get_sae_acts(batch_inputs, layer=layer, agg=agg)
+            start = i * batch_size
+            end = start + batch_feats.shape[0]
+            sae_mat[start:end] = batch_feats.cpu()
+
+    # torch.cuda.empty_cache()
+    # gc.collect()
+
+    return sae_mat
 
 def get_reasoning_features(trivia_examples=250, k=10, model_name='deepseek-ai/DeepSeek-R1-Distill-Llama-8B', sae_release="llama_scope_r1_distill", sae_id= "l25r_400m_slimpajama_400m_openr1_math", sae_layer=25):
-    # Todo: make these customizable
+
     model, tokenizer = load_model(model_name)
     sae = load_sae(sae_release, sae_id)
 
     aqua_ds = load_aqua()
     trivia_ds = load_trivia()
 
-    aqua_means = torch.zeros((aqua_ds.num_rows, sae.cfg.d_sae))
+    aq_prompts = [format_prompt_aqua(q, reasoning=False, include_options=False) for q in aqua_ds['question']]
+    aq_tokenized = tokenizer(aq_prompts, return_tensors='pt', padding=True, truncation=True)
+    aq_collate_fn = partial(collate_tokenized, tokenized=aq_tokenized)
 
-    for index, question in enumerate(tqdm(aqua_ds)):
-        prompt = format_prompt_aqua(question, reasoning=False, include_options=False)
-        inputs = tokenizer(prompt, return_tensors='pt').to(model.device)
-        aqua_means[index, :] = get_sae_acts(inputs, sae_layer)
+    aqua_means = get_ds_saes(sae, sae_layer, aq_prompts, model, collate_fn=aq_collate_fn, agg='mean')
 
-    trivia_means = torch.zeros((trivia_examples, sae.cfg.d_sae))
+    tr_prompts = [format_prompt_trivia(q, reasoning=False) for q in trivia_ds[:trivia_examples]['question']]
+    tr_tokenized = tokenizer(tr_prompts, return_tensors='pt', padding=True, truncation=True)
+    tr_collate_fn = partial(collate_tokenized, tokenized=tr_tokenized)
 
-    for index, question in enumerate(tqdm(trivia_ds[:trivia_examples]['question'])):
-        prompt = format_prompt_trivia(question, reasoning=False)
-        inputs = tokenizer(prompt, return_tensors='pt').to(model.device)
-        trivia_means[index, :] = get_sae_acts(inputs, sae_layer)
+    trivia_means = get_ds_saes(sae, sae_layer, tr_prompts, model, collate_fn=tr_collate_fn, agg='mean')
 
-    mean_aqua = trim_mean(aqua_means.detach(), proportiontocut=0.05, axis=0)
-    mean_trivia = trim_mean(trivia_means.detach(), proportiontocut=0.05, axis=0)
+    aqua_means = trim_mean(aqua_means.detach(), proportiontocut=0.05, axis=0)
+    trivia_means = trim_mean(trivia_means.detach(), proportiontocut=0.05, axis=0)
 
     epsilon = 1e-6
-    percentage_increase = 100 * (mean_aqua - mean_trivia) / (mean_trivia + epsilon)
+    percentage_increase = 100 * (aqua_means - trivia_means) / (trivia_means + epsilon)
 
-    valid = (mean_trivia > 0.01) & (mean_aqua > 0.1)
+    valid = (trivia_means > 0.01) & (aqua_means > 0.1)
     filtered_percentage_increase = percentage_increase[valid]
     valid_indices = np.where(valid)[0]
 
--- src/sae.py
+++ src/sae.py
@@ -4,16 +4,34 @@ import torch
 device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
 
 def load_sae(release, sae_id):
-    sae = SAE.from_pretrained_with_cfg_and_sparsity(release, sae_id)
+    sae = SAE.from_pretrained(release, sae_id)
     sae = sae.to(device)
     return sae
 
-def get_sae_acts(model, sae, text, layer, agg='mean'):
+def get_sae_acts(model, sae, input_batch, layer, agg='mean'):
+
+    activation_dict = {}
+
+    def hook_fn(module, input, output):
+        activation_dict["hidden"] = output
+
+    hook = model.model.layers[layer].register_forward_hook(hook_fn)
+
+    model.eval()
+
     with torch.no_grad():
-        outputs = model(**text, output_hidden_states=True, return_dict=True)
-    hidden_states = outputs.hidden_states
-    raw_feats = sae.encode(hidden_states[layer])
+        _ = model(**input_batch)
+
+    hook.remove()
+
+    hidden_states = activation_dict['hidden']
+    raw_feats = sae.encode(hidden_states)
+
     if agg == 'mean':
-        return raw_feats[0].mean(axis=0)
+        result= raw_feats.mean(dim=1)
     elif agg == 'last':
-        return raw_feats[0, -1]
+        result = raw_feats[:, -1]
+
+    del hidden_states, raw_feats
+
+    return result
