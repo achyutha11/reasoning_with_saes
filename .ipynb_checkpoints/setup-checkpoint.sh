898abba4343edd1cf7fbd69c2f3e42b5dd50876e
Achu Nautilus <a2menon@ucsd.edu>
2025-08-27 20:22:38 -0700

    small updates

--- .ipynb_checkpoints/setup-checkpoint.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-apt-get update && apt-get install -y build-essential git curl
-pip install --upgrade pip
-pip install transformers accelerate datasets bitsandbytes
-pip install nltk
-pip install git+https://github.com/TransformerLensOrg/TransformerLens
-pip install sae-lens
