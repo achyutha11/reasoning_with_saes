da098da4f4eca7e20f4616e5041f834c927e36f9
Achu Menon <achyutha11@gmail.com>
2025-09-30 18:33:21 -0700

    Delete src/.ipynb_checkpoints directory

--- src/.ipynb_checkpoints/get_datasets-checkpoint.py
+++
@@ -1,7 +0,0 @@
-from datasets import load_dataset
-
-def load_aqua(split='test'):
-    return load_dataset('aqua_rat', 'raw', split=split)
-
-def load_trivia(split='test'):
-    return load_dataset("mandarjoshi/trivia_qa", "rc", split=split)
